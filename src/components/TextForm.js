28cae0dab1ad2e55102a7d9137b798453b194e8b
NamitKumar16 <kumarnamit58@gmail.com>
2022-06-11 22:10:21 +0530

    Added Dark Mode and more functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,28 +1,67 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter Text Here");
+  const [text, setText] = useState("");
   const handleUpClick = () => {
     setText(text.toUpperCase());
+  };
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+  };
+  const handleClearClick = () => {
+    setText("");
+  };
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
   };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} Words, {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read this</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
